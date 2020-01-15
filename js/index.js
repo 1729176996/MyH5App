d4e5adba9b253ef38df408ab875946b7f276947d
1729176996 <1729176996@qq.com>
2020-01-15 10:12:24 +0800

    4

--- js/index.js
+++ js/index.js
@@ -8,6 +8,62 @@ $(function(){
 	    },
 	    mounted:function(){
 			this.init();
+			if(getBrowser()!='电脑端'){
+				// H5 plus事件处理
+				function plusReady(){
+					// 检查当前版本，与从后台获取的版本作比较，以此判断是否更新     
+					plus.runtime.getProperty(plus.runtime.appid,function(inf){
+						// 当前版本
+						var wgtVersion = inf.version;
+						mui.ajax("https://1729176996.github.io/MyH5App/update/config.json", {
+							dataType: 'json',
+							type: 'get',
+							success:function(data){
+								// 如果有新版本，则提示需要更新
+								if( data.version > wgtVersion ){
+									mui.confirm('检查更新','发现新版本，是否更新',['确定','取消'],function(e){
+										if(e.index==0){
+											downloadWgt(); // 下载wgt方法
+										}else{
+											return;
+										}
+									})
+								}else{
+									return;
+								}
+							},
+							error:function(err){}
+						});
+					});
+				}
+				if(window.plus){
+					plusReady();
+				}else{
+					document.addEventListener('plusready',plusReady,false);
+				}
+			}else{
+				/*mui.ajax("https://1729176996.github.io/MyH5App/update/config.json", {
+					dataType: 'json',
+					type: 'get',
+					success:function(data){
+						console.log(data);
+					},
+					error:function(err){
+						console.log(err);
+					}
+				});*/
+				$.ajax({
+					url:'https://1729176996.github.io/MyH5App/update/version.json',
+					type:'get',
+					dataType: 'json',
+					success:function(data){
+						console.log(data);
+					},
+					error:function(xhr, errorType, error,msg){
+						mui.alert(msg,'提示','确定',null,'div');
+					}
+				})
+			}
 	    },
 	    methods:{
 			init:function(){
