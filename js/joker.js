a9fa3fd38b23f4938c5b7037ef36ee44902cc788
1729176996 <1729176996@qq.com>
2020-01-16 10:42:42 +0800

    2

--- js/joker.js
+++ js/joker.js
@@ -10,19 +10,18 @@ $(function(){
 			refreshType:'',//上拉或下拉
 	    },
 	    mounted:function(){
-			this.type = 'dtgxt';
 			this.init();
+			this.changgeType('dtgxt');
 	    },
 	    methods:{
 			init:function(){
 				var _this = this;
-				loading = weui.loading("加载中");
 				mui.init({
 					pullRefresh : {
 						container:"#scrollWrapper",//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
 						down : {
 							height:50,//可选,默认50.触发下拉刷新拖动距离,
-							auto: true,//可选,默认false.首次加载自动下拉刷新一次
+							auto: false,//可选,默认false.首次加载自动下拉刷新一次
 							contentdown : "下拉可以刷新",//可选，在下拉可刷新状态时，下拉刷新控件上显示的标题内容
 							contentover : "释放立即刷新",//可选，在释放可刷新状态时，下拉刷新控件上显示的标题内容
 							contentrefresh : "正在刷新...",//可选，正在刷新状态时，下拉刷新控件上显示的标题内容
@@ -83,10 +82,11 @@ $(function(){
 							page--;
 							mui.alert('查询错误','提示','确定',null,'div');
 						}
+						_this.$forceUpdate();
 						_this.$nextTick(function(){
 							if(_this.refreshType=='下拉'){
 								mui('#scrollWrapper').pullRefresh().endPulldownToRefresh();
-								mui('#scrollWrapper').pullRefresh().scrollTo(0,0,100);
+								mui('#scrollWrapper').pullRefresh().scrollTo(0,0,0);
 							}else if(_this.refreshType=='上拉'){
 								mui('#scrollWrapper').pullRefresh().endPullupToRefresh();
 							}
@@ -100,7 +100,7 @@ $(function(){
 						page--;
 						if(_this.refreshType=='下拉'){
 							mui('#scrollWrapper').pullRefresh().endPulldownToRefresh();
-							mui('#scrollWrapper').pullRefresh().scrollTo(0,0,100);
+							mui('#scrollWrapper').pullRefresh().scrollTo(0,0,0);
 						}else if(_this.refreshType=='上拉'){
 							mui('#scrollWrapper').pullRefresh().endPullupToRefresh();
 						}
